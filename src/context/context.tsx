2b8e51466e63206e38cda1d85ed32067030d1032
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-10-30 17:20:06 +0530

    fix: removed filter context

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -2,10 +2,6 @@
 
 import { createContext, ReactNode, useState } from "react";
 
-export const filterContext = createContext<{
-  sortOpen: boolean;
-  setSortOpen: React.Dispatch<React.SetStateAction<boolean>>;
-} | null>(null);
 export const sidebarContext = createContext<{
   sidebarOpen: boolean;
   setSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -13,13 +9,10 @@ export const sidebarContext = createContext<{
 
 export function Context({ children }: { children: ReactNode }) {
   const [sidebarOpen, setSidebarOpen] = useState(true);
-  const [sortOpen, setSortOpen] = useState(false);
 
   return (
     <sidebarContext.Provider value={{ sidebarOpen, setSidebarOpen }}>
-      <filterContext.Provider value={{ sortOpen, setSortOpen }}>
-        {children}
-      </filterContext.Provider>
+      {children}
     </sidebarContext.Provider>
   );
 }
