be9ff2593ecaa6c562fda92e4b947f1c8e799593
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-10-30 23:27:14 +0530

    fix: improve the accessibility for the product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -15,26 +15,38 @@ export default function ProductCard({
 
   return (
     <>
-      <article className="product-card">
+      <article
+        className="product-card"
+        aria-labelledby={`product-title-${product._id}`}
+        aria-describedby={`product-info-${product._id}`}
+        role="region"
+      >
         <div className="product-image">
           <Image
             src={product?.image}
             alt={product?.name}
             width={300}
             height={400}
-            className={`${outOfStock ? "image-opacity-low" : ""}`}
-            layout="responsive"
+            className={`product-image-img ${
+              outOfStock ? "image-opacity-low" : ""
+            }`}
           />
           {product?.markedAsNew && (
-            <span className="product-tag">new product</span>
+            <span className="product-tag" role="status">
+              new product
+            </span>
           )}
 
           {outOfStock && (
-            <span className="product-tag-stock-out">out of stock</span>
+            <span className="product-tag-stock-out" role="status">
+              out of stock
+            </span>
           )}
         </div>
-        <div className="product-info">
-          <h2 className="product-title">{product?.name}</h2>
+        <div className="product-info" id={`product-info-${product._id}`}>
+          <h2 className="product-title" id={`product-title-${product._id}`}>
+            {product?.name}
+          </h2>
           <div className="product-info-wrapper">
             <p className="product-price">
               <span>Sign in</span> or Create an account to see pricing
@@ -42,7 +54,7 @@ export default function ProductCard({
             <div className="product-rating">
               <Image
                 src="/assets/svg/heart.svg"
-                alt="wishlist"
+                alt="Add to wishlist"
                 width={24}
                 height={24}
               />
